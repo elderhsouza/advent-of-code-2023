d2e852b436140ec00cb63026b390376975790a10
Elder Henrique Souza <elderhenrique@gmail.com>
2023-12-02 14:03:17 -0300

    feat: day 2 part 2

--- day-2/main.ts
+++ day-2/main.ts
@@ -1,18 +1,18 @@
 import fs from "fs";
 
-type Colors = "red" | "green" | "blue";
+type CubeColors = "red" | "green" | "blue";
 interface GetPossibleGamesArgs {
   input: string;
-  maxCubes: Record<Colors, number>;
+  maxCubes: Record<CubeColors, number>;
 }
 
 function getPossibleGamesPartOne(args: GetPossibleGamesArgs) {
   let sum = 0;
 
-  for (const [id, line] of args.input.split("\n").entries()) {
+  for (const [id, game] of args.input.split("\n").entries()) {
     let isImpossible = false;
 
-    const sets = line.match(
+    const sets = game.match(
       /(\d+\s(?:red|green|blue)(?:,\s)*)+/g
     ) as RegExpMatchArray;
 
@@ -24,7 +24,7 @@ function getPossibleGamesPartOne(args: GetPossibleGamesArgs) {
           /(\d+)\s(\w+)/
         ) as RegExpMatchArray;
 
-        if (Number(quantity) > args.maxCubes[color as Colors]) {
+        if (Number(quantity) > args.maxCubes[color as CubeColors]) {
           isImpossible = true;
           break;
         }
@@ -36,13 +36,45 @@ function getPossibleGamesPartOne(args: GetPossibleGamesArgs) {
     }
   }
 
-  return sum;
-} // 3099
+  return sum; // test: 8 | full: 3099
+}
+
+function getPossibleGamesPartTwo(args: GetPossibleGamesArgs) {
+  let sum = 0;
+
+  for (const game of args.input.split("\n")) {
+    const cache: Record<CubeColors, number[]> = { red: [], green: [], blue: [] };
+
+    const sets = game.match(
+      /(\d+\s(?:red|green|blue)(?:,\s)*)+/g
+    ) as RegExpMatchArray;
+
+    for (const hand of sets.map((set) => set.split(","))) {
+      for (const cubes of hand) {
+        const { 1: quantity, 2: color } = cubes.match(
+          /(\d+)\s(\w+)/
+        ) as RegExpMatchArray;
+
+        cache[color as CubeColors].push(Number(quantity));
+      }
+    }
+    sum += Math.max(...cache.red) * Math.max(...cache.green) * (Math.max(...cache.blue));
+  }
+  return sum; // test: 2286 | full: 72970
+}
 
 fs.readFile(`${__dirname}/input.txt`, "utf8", (err, data) => {
   if (err) throw err;
+
   console.log(
     getPossibleGamesPartOne({
+      input: data,
+      maxCubes: { red: 12, green: 13, blue: 14 },
+    })
+  );
+
+  console.log(
+    getPossibleGamesPartTwo({
       input: data,
       maxCubes: { red: 12, green: 13, blue: 14 },
     })
